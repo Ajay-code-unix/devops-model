9a76cc87e7e5841a35d0e8551dd84da7d21fd9c5
Ajay-code-unix <58654765+Ajay-code-unix@users.noreply.github.com>
2024-11-06 14:12:53 +0530

    Update server.js stage

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express=require('express')
 const app = express();
 const PORT=3000;
-app.get('/',(req,res)=> res.send('Hello staginggg 123'));
+app.get('/',(req,res)=> res.send('Hello staginggg :)123'));
 app.listen(PORT,()=>console.log(`APP running on Port ${PORT}`));
